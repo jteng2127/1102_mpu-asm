2e9543b267a714c306eaceeb54d864ee94b25393
WeiDai2127 <39561523+WeiDai2127@users.noreply.github.com>
2022-06-06 02:21:44 +0800

    modify lab10

--- group18_lab10.asm
+++ group18_lab10.asm
@@ -1,3 +1,4 @@
+; 5. 在你的程式之中, 是否需要清除溢位旗標? NO
   ORG  0H           ;系統開機的執行位址為0
   JMP  MAIN
   ORG  000BH
@@ -12,16 +13,23 @@ MAIN:
   MOV TMOD,  #01H ;Timer 0, mode 1
   SETB TR0
   SETB TF0
-  MOV IE,  #82H
-  CJNE R1, #10,  $
-  MOV R1, #0
-  JMP MAIN
+  MOV IE, #82H
+  SJMP $
 
 T0ISR:  ;Timer0 ISR
+  MOV TL0, #0B0H ; |
+  MOV TH0, #03CH ; | set timer to 65536 - 50000
+  DEC R3
+  CJNE R3, #0, ISREND
+SHOW:
+  MOV R3, #2
   MOV A, R1
   MOVC A, @A+DPTR
   MOV P1, A
   INC R1
+  CJNE R1, #10, ISREND
+  MOV R1, #0
+ISREND:
   RETI
 
 TABLE:
@@ -29,6 +37,3 @@ TABLE:
   DB 99H, 92H, 82H, 0F8H
   DB 80H, 98H
   END
-
-
-
